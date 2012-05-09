44f8cc906d81766d8633447db92e99d0a28646f5
Mukesh Bhati <bhati.mukesh@gail.com>
2012-05-09 10:47:43 -0400

    Add a Title Helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,6 +14,13 @@ describe "Static pages" do
       page.should have_selector('title',
                         :text => "Ruby on Rails Tutorial Sample App | Home")
     end
+
+    it "should not have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title',
+                        :text => "| Home")
+    end  
+
   end
 
   describe "Help page" do
